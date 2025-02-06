501c2db917196c40311130bc1a0811b0605cccd3
jaymarc21 <custodiojaymarc_bsit@plmun.edu.ph>
2025-02-06 21:51:54 +0800

    Add files via upload

---
+++ exercise_01.py
@@ -0,0 +1,13 @@
+count = 10                  
+decimal_value = 3.14  
+message = "Hello, Python!"
+is_active = True 
+result = None
+count = 10                    
+total_count = 20     
+print("Integer (count):", count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
